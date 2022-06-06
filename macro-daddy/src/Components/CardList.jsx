bfaa98aac8ba1939a8be13d1649e3228842abecb
Quoc <LamQuoc.24@gmail.com>
2022-06-06 16:27:22 -0400

    render bmr

--- macro-daddy/src/Components/CardList.jsx
+++ macro-daddy/src/Components/CardList.jsx
@@ -2,31 +2,30 @@ import React, { useEffect } from "react";
 import axios from "axios";
 
 const CardList = ({ userStats, bmr, setBmr }) => {
-
-    useEffect(() => {
-        if (userStats.gender === "female") {
-          setBmr(
-            655 +
-            4.35 * userStats.weight +
-            4.7 * userStats.height -
-            4.7 * parseInt(userStats.age)
-            );
-          } else {
-            setBmr(
-              66 +
-              6.23 * userStats.weight +
-              12.7 * userStats.height -
-              6.8 * userStats.age
-              );
-            }
-          }, []);
-          console.log(bmr);
+  useEffect(() => {
+    if (userStats.gender === "female") {
+      setBmr(
+        655 +
+          4.35 * userStats.weight +
+          4.7 * userStats.height -
+          4.7 * parseInt(userStats.age)
+      );
+    } else {
+      setBmr(
+        66 +
+          6.23 * userStats.weight +
+          12.7 * userStats.height -
+          6.8 * userStats.age
+      );
+    }
+  }, []);
+  console.log(bmr);
 
   return (
     <div className="cardlist-parent">
       <div className="macro-display">
         <p>Age: {userStats.age}</p>
-        <p>Calories:</p>
+        <p>Calories: {bmr}</p>
         <p>Proteins:</p>
         <p>Carbs:</p>
         <p>Fats:</p>
